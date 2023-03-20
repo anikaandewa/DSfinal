b944d0d23ec5e7bb9c62a2b5c8c3d9420e22af21
anikaandewa <126886443+anikaandewa@users.noreply.github.com>
2023-03-20 18:34:23 +0100

    Update dmAppointment.ts

--- src/dmAppointment.ts
+++ src/dmAppointment.ts
@@ -2,7 +2,6 @@ import { MachineConfig, send, Action, assign } from "xstate";
 import { LEMMAS } from "./wordbank"
 import { LEGAL } from "./wordbank"
 
-
 function say(text: string): Action<SDSContext, SDSEvent> {
   return send((_context: SDSContext) => ({ type: "SPEAK", value: text }));
 }
@@ -18,46 +17,50 @@ const findWord = (context: SDSContext, entity: string) => {
   let siupsiup = LEMMAS.filter((k) => k[0] === psi) // retrieves words from lemmas that start with the letter in psi
   const randIndex = Math.floor(Math.random() * siupsiup.length); // gives a random index among the retrieved words
   const word = siupsiup[randIndex];
-  console.log(word)
   let unusedWords = LEMMAS.filter(word => !listOfUsedWords.includes(word))
   if (listOfUsedWords.includes(word))
   {
     if (!((unusedWords.filter(word => word.startsWith(psi))).length === 0)) {
     let word1 = unusedWords[Math.floor(Math.random() * unusedWords.length)]
     listOfUsedWords.push(word1)
-    return word1
+    return [word1, (listOfUsedWords.length/2)]
   }
   return "I cannot think of anything else. You win."
 }
   else {
     listOfUsedWords.push(word)
-    return word
+    return [word, (listOfUsedWords.length/2)]
   }
 };
-let userScore: string[] = []
 
 const findErrors = (context: SDSContext, entity: string) => {
   // lowercase the utterance and remove tailing "."
   let u = context.recResult[0].utterance.toLowerCase().replace(/\.$/g, "");
-  userScore.push(u)
   console.log(listOfUsedWords)
-  const score = userScore.length
   if (listOfUsedWords.length > 0) {
     let lastWord = listOfUsedWords[listOfUsedWords.length - 1];
   let lastLetter = lastWord[lastWord.length - 1];
   let firstLetter = u[0];
   if (!LEGAL.includes(u)) {
-    return "not legal";
+    return ["not legal", (listOfUsedWords.length/2)];
   } 
-  if (firstLetter !== lastLetter) {
-    return "no match";
+  else if (firstLetter !== lastLetter) {
+    return ["no match", (listOfUsedWords.length/2)];
   }
-  if (u.split(' ').length > 1) {
-    return "too many words";
+  else if (u.split(' ').length > 1) {
+    return ["too many words", (listOfUsedWords.length/2)];
   } else {
-}
-}
   return false
+}
+  }
+  if (!LEGAL.includes(u)) {
+    return ["not legal", (listOfUsedWords.length/2)];
+  }
+  else if (u.split(' ').length > 1) {
+    return ["too many words", (listOfUsedWords.length/2)];
+  } else {
+  return false
+}
 };
 
 export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
@@ -86,16 +89,16 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
             }),
           },
           {
-            target: "endgame_toomanywords",
-            cond: (context) => findErrors(context, "title") === "too many words",
+            target: "endgame_toomanywords_start",
+            cond: (context) => findErrors(context, "title")[0] === "too many words",
           },
           {
-            target: "endgame_nomatch",
-            cond: (context) => findErrors(context, "title") === "no match",
+            target: "endgame_nomatch_start",
+            cond: (context) => findErrors(context, "title")[0] === "no match",
           },
           {
-            target: "endgame_illegal",
-            cond: (context) => findErrors(context, "title") === "not legal",
+            target: "endgame_illegal_start",
+            cond: (context) => findErrors(context, "title")[0] === "not legal",
           },
           {
             target: ".nomatch",
@@ -126,33 +129,41 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
         RECOGNISED: [
           {
             target: "giveword2",
+            cond: (context) => findErrors(context, "title") === false,
             actions: assign({
               title: (context) => findWord(context, "title"),
             }),
           },
           {
             target: "endgame_toomanywords",
-            cond: (context) => findErrors(context, "title") === "too many words",
+            cond: (context) => findErrors(context, "title")[0] === "too many words",
           },
           {
             target: "endgame_nomatch",
-            cond: (context) => findErrors(context, "title") === "no match",
+            cond: (context) => findErrors(context, "title")[0] === "no match",
           },
           {
             target: "endgame_illegal",
-            cond: (context) => findErrors(context, "title") === "not legal",
+            cond: (context) => findErrors(context, "title")[0] === "not legal",
           },
           {
             target: ".nomatch",
           },
         ],
-        TIMEOUT: ".prompt",
+        TIMEOUT: "#outOfTime",
       },
       states: {
+        outOfTime: {
+          id: "outOfTime",
+          entry: send((context) => ({
+              type: "SPEAK",
+              value: `Oh-oh. You ran out of time. You lose! Your score is ${context.title[1]}.`,
+            })),
+            },
         prompt: {
           entry: send((context) => ({
             type: "SPEAK",
-            value: `${context.title}`,
+            value: `${context.title[0]}`,
           })),
           on: { ENDSPEECH: "ask" },
         },
@@ -174,33 +185,34 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
         RECOGNISED: [
           {
             target: "giveword",
+            cond: (context) => findErrors(context, "title") === false,
             actions: assign({
               title: (context) => findWord(context, "title"),
             }),
           },
           {
             target: "endgame_toomanywords",
-            cond: (context) => findErrors(context, "title") === "too many words",
+            cond: (context) => findErrors(context, "title")[0] === "too many words",
           },
           {
             target: "endgame_nomatch",
-            cond: (context) => findErrors(context, "title") === "no match",
+            cond: (context) => findErrors(context, "title")[0] === "no match",
           },
           {
             target: "endgame_illegal",
-            cond: (context) => findErrors(context, "title") === "not legal",
+            cond: (context) => findErrors(context, "title")[0] === "not legal",
           },
           {
             target: ".nomatch",
           },
         ],
-        TIMEOUT: ".prompt",
+        TIMEOUT: "#outOfTime",
       },
       states: {
         prompt: {
           entry: send((context) => ({
             type: "SPEAK",
-            value: `${context.title}`,
+            value: `${context.title[0]}`,
           })),
           on: { ENDSPEECH: "ask" },
         },
@@ -218,19 +230,37 @@ export const dmMachine: MachineConfig<SDSContext, any, SDSEvent> = {
     endgame_toomanywords: {
       entry: send((context) => ({
         type: "SPEAK",
-        value: `You can only respond with one word! You lose. Your score is ${context.title}`,
+        value: `You can only respond with one word! You lose. Your score is ${context.title[1]}`,
       })),
     },
     endgame_nomatch: {
       entry: send((context) => ({
         type: "SPEAK",
-        value: `Your word did not match mine! You lose. Your score is ${context.title}`,
+        value: `Your word did not match mine! You lose. Your score is ${context.title[1]}`,
       })),
     },
     endgame_illegal: {
       entry: send((context) => ({
         type: "SPEAK",
-        value: `I do not have this word in my database! You lose. Your score is ${context.title}`,
+        value: `I do not have this word in my database! You lose. Your score is ${context.title[1]}`,
+      })),
+    },
+    endgame_toomanywords_start: {
+      entry: send((context) => ({
+        type: "SPEAK",
+        value: `You can only respond with one word! You lose. Your score is 0`,
+      })),
+    },
+    endgame_nomatch_start: {
+      entry: send((context) => ({
+        type: "SPEAK",
+        value: `Your word did not match mine! You lose. Your score is 0`,
+      })),
+    },
+    endgame_illegal_start: {
+      entry: send((context) => ({
+        type: "SPEAK",
+        value: `I do not have this word in my database! You lose. Your score is 0`,
       })),
     },
   },
